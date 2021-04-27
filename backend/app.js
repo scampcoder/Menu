2429586d75d7b718c84ef40492399609671c15d9
Victoria Kee <victoriakee95@gmail.com>
2021-04-27 12:51:47 -0500

    Refactor

--- backend/app.js
+++ backend/app.js
@@ -75,20 +75,24 @@ const menu = [
 
   const sectionCenter = document.querySelector('.section-center');
   window.addEventListener('DOMContentLoaded', function(){
-    let displayMenu = menu.map(function(item){
+    displayMenuItems(menu);
+  });
+
+  function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function(item){
       return `<article class="menu-item">
-      <img src=${item.img} alt=${item.title}>
-      <div class="item-info">
-          <header>
-              <h4>${item.title}</h4>
-              <h4 class="price">${item.price}</h4>
-          </header>
-          <p class="item-text">
-              ${item.desc}
-          </p>
-      </div>
-  </article>`;
+                <img src=${item.img} alt=${item.title}>
+                <div class="item-info">
+                    <header>
+                        <h4>${item.title}</h4>
+                        <h4 class="price">${item.price}</h4>
+                    </header>
+                    <p class="item-text">
+                        ${item.desc}
+                    </p>
+                </div>
+              </article>`;
     });
     displayMenu = displayMenu.join("")
     sectionCenter.innerHTML = displayMenu;
-  });
+  };
